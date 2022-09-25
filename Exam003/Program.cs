973aeac24d9c661e23b205a51e8114ffcd946a4b
alexandravdeenko <alex-avdeenko@yandex.ru>
2022-09-25 21:08:14 +0700

    Метод, который принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N

---
+++ Exam003/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Введите число: ");
+int cube = int.Parse(Console.ReadLine());
+
+void Cube(int[] cube)
+{
+  int count = 1;
+  int length = cube.Length;
+  while (count <  length)
+  {
+    cube[count] = Convert.ToInt32(Math.Pow(count, 3));
+    count++;
+  }
+}
+
+void Print(int[] array)
+{
+    int size = array.Length;
+    int i = 1;
+
+    while (i < size)
+    {
+        Console.Write(array[i] + " ");
+        i ++;
+    }
+    Console.WriteLine();
+} 
+
+int[] col = new int[cube + 1];
+Cube(col);
+Print(col);
