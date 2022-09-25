0da224eebaf7c2bd1e05e131ef6f768d5ece711c
alexandravdeenko <alex-avdeenko@yandex.ru>
2022-09-25 20:56:47 +0700

    Метод, который принимает на вход пятизначное число и проверяет, является ли оно палиндромом

---
+++ Exam001/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите пятизначное число: ");
+string number = Console.ReadLine();
+
+void Palindrome(string number)
+{
+  if (number[0]==number[4] && number[1]==number[3])
+  {
+    Console.WriteLine("Число является палиндромом");
+  }
+  else
+  {
+    Console.WriteLine("Число не является палиндромом");
+  }
+}
+
+if (number.Length == 5)
+{
+  Palindrome(number);
+}
+else Console.WriteLine("Введите правильное число");
